d1097947eca5c8d55b205d350b6cf80333014caf
joshuatimi54 <105829140+joshuatimi54@users.noreply.github.com>
2022-10-14 04:14:43 +0100

    kalos

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,11 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.backgroundColor = "rgb(189,200,242)";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "rgb(189,200,242)";
+  }
+};
