f25b545bf79876d2693eebe7df9bac4753277678
joshuatimi54 <105829140+joshuatimi54@users.noreply.github.com>
2022-10-15 22:15:40 +0100

    kalos

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,13 +1,14 @@
 function myFunction(x) {
   x.classList.toggle("change");
-}
+};
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.documentElement.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("navbar").style.backgroundColor = "rgb(189,200,242)";
+  if (document.bdy.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("topbar").style.backgroundColor = "rgb(189,200,242)";
+    document.getElementById("topbar").style.transition = "1s";
   } else {
-    document.getElementById("navbar").style.backgroundColor = "rgb(189,200,242,0)";
+    document.getElementById("topbar").style.backgroundColor = "rgb(189,200,242,0)";
   };
 };
